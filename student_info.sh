d3452e1dcaa62acfbbebb748edc7cbf351966af9
Anant Kailash Laghane <98004476+anantlaghane@users.noreply.github.com>
2025-02-11 15:22:52 +0530

    Update student_info.sh

--- student_info.sh
+++ student_info.sh
@@ -1,12 +1,31 @@
-#!/bin/bash 
+# #!/bin/bash 
 
-if [ $# -ne 2 ]; then 
-    echo "Usage: $0 <student_name> <city>"
-    exit 1
-fi
+# if [ $# -ne 2 ]; then 
+#     echo "Usage: $0 <student_name> <city>"
+#     exit 1
+# fi
 
-STUDENT_NAME=$1
-CITY=$2    
+# STUDENT_NAME=$1
+# CITY=$2    
 
-echo "Student Name: $STUDENT_NAME"
-echo "City: $CITY"
+# echo "Student Name: $STUDENT_NAME"
+# echo "City: $CITY"
+
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch: 'main', url: 'https://github.com/anantlaghane/shell-script1.git'
+            }
+        }
+
+        stage('Run Shell Script') {
+            steps {
+                sh 'chmod +x student_info.sh && ./student_info.sh Anant Pune'
+            }
+        }
+    }
+}
